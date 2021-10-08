c56b88fc28dbeda63769774d804fa7d73da0b745
Tananat-Kometjamikorn <tananat.k@ku.th>
2021-10-08 12:10:51 +0700

    Initial APP

---
+++ src/main/java/th/ac/ku/WashingOfficeApplication/WashingOfficeApplication.java
@@ -0,0 +1,13 @@
+package th.ac.ku.WashingOfficeApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashingOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WashingOfficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/th/ac/ku/WashingOfficeApplication/WashingOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package th.ac.ku.WashingOfficeApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WashingOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
