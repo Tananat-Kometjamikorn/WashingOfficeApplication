545b171be4fd6ac58aa87378e4b87a80f24ff2a8
Tananat-Kometjamikorn <tananat.k@ku.th>
2021-10-13 21:29:18 +0700

    Update model and Start create APIService

--- src/main/java/th/ac/ku/app/models/User.java
+++ src/main/java/th/ac/ku/app/models/User.java
@@ -1,15 +1,47 @@
 package th.ac.ku.app.models;
 
-public class
-User {
+public class User {
     private String userName;
     private String password;
     private String name;
 
+    public int changePassword(String currentPassword, String newPassword, String confirmNewPassword) {
+        if (currentPassword.equals(password)) {
+            if (newPassword.equals(confirmNewPassword)) {
+                password = newPassword;
+                return 0;
+            }
+            else{
+                return 1;
+            }
+        }
+        else{
+            return 2;
+        }
+    }
 
-    public User(String userName, String password, String name) {
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
         this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 }
+
