35266887e8daa7c42f84c948e55a94f13b72f88d
Tananat-Kometjamikorn <tananat.k@ku.th>
2021-10-14 22:49:20 +0700

    Update login controller

--- src/main/java/th/ac/ku/app/controller/login/LoginController.java
+++ src/main/java/th/ac/ku/app/controller/login/LoginController.java
@@ -4,39 +4,64 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import org.springframework.stereotype.Component;
 import th.ac.ku.app.controller.branch.BranchPageController;
 import th.ac.ku.app.controller.hq.HqPageController;
 
 import java.io.IOException;
 
-public class LoginController {
+@Component
+public class LoginController{
+
     @FXML private Button loginBtn;
-    @FXML private TextField usernameField, passwordField;
+    @FXML private TextField usernameField;
+    @FXML private PasswordField passwordField;
+    @FXML private RadioButton branchBtn,headQuarterBtn;
+
+    private Alert alert;
+    private ToggleGroup roleToggleGroup = new ToggleGroup();
+
+
+    @FXML public void initialize() {
+        this.branchBtn.setToggleGroup(roleToggleGroup);
+        this.headQuarterBtn.setToggleGroup(roleToggleGroup);
+        branchBtn.setSelected(true);
+    }
 
     @FXML
     public void handleLoginBtnOnAction(ActionEvent event) throws IOException {
 
-
-//        Button b = (Button) event.getSource();
-//        Stage stage = (Stage) b.getScene().getWindow();
-//        FXMLLoader loader = new FXMLLoader(
-//                getClass().getResource("/branch_page.fxml")
-//        );
-//        stage.setScene(new Scene(loader.load(), 1024, 720));
-//        BranchPageController branch = loader.getController();
-//        stage.show();
-
-
-        Button b = (Button) event.getSource();
-        Stage stage = (Stage) b.getScene().getWindow();
-        FXMLLoader loader = new FXMLLoader(
-                getClass().getResource("/hq_page.fxml")
-        );
-        stage.setScene(new Scene(loader.load(), 1024, 720));
-        HqPageController branch = loader.getController();
-        stage.show();
+        if (this.roleToggleGroup.getSelectedToggle().equals(this.branchBtn)) {
+            Button b = (Button) event.getSource();
+            Stage stage = (Stage) b.getScene().getWindow();
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("/branch_page.fxml")
+            );
+            stage.setScene(new Scene(loader.load(), 1024, 720));
+            BranchPageController branch = loader.getController();
+            stage.show();
+        }
+        else if (this.roleToggleGroup.getSelectedToggle().equals(this.headQuarterBtn)) {
+            Button b = (Button) event.getSource();
+            Stage stage = (Stage) b.getScene().getWindow();
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("/hq_page.fxml")
+            );
+            stage.setScene(new Scene(loader.load(), 1024, 720));
+            HqPageController hq = loader.getController();
+            stage.show();
+            }
     }
+    private void AlertBox(){
+        alert = new Alert(Alert.AlertType.ERROR);
+        alert.initStyle(StageStyle.UTILITY);
+        alert.setTitle("ERROR");
+        alert.setHeaderText(null);
+        alert.setContentText("kuy");
+        alert.showAndWait();
+    }
+
 }
--- src/main/java/th/ac/ku/app/models/UserBranch.java
+++ src/main/java/th/ac/ku/app/models/UserBranch.java
@@ -5,5 +5,10 @@ import java.util.Set;
 
 public class UserBranch extends User {
 
+    public UserBranch(String userName, String password, String name) {
+        super(userName, password, name);
+    }
     private Set<OrderInfo> orderInfos = new HashSet<>();
+
+
 }
